a6ba6ae4a07dcba34ceb7c1d986fae43bd5bafaa
pappala_naidu <papppala_naidu@persistent.com>
2023-08-23 16:12:38 +0000

    new commit

---
+++ Translated_PGSQL/source/stored_procedures/spLockAsyncListBuild.sql
@@ -0,0 +1,38 @@
+CREATE PROCEDURE [dbo].[spLockAsyncListBuild] (
+  _EmailListID INT,
+  _ProcedureName NVARCHAR(256),
+  _ExternalUniqueID UNIQUEIDENTIFIER = NULL,
+  _LockID INT = -1 OUTPUT)
+AS
+BEGIN
+  SET
+    NOCOUNT ON;
+
+  -- LOCK TYPES
+  DECLARE _LockTypeListBuild INT = 1;
+
+  DECLARE _LockTypeAsyncProcess INT = 2;
+
+  -- IS LOCKED
+  DECLARE _IsLockedAsyncProc BIT = 1;
+
+  BEGIN TRANSACTION;
+
+  -- CHECK LOCKED
+  EXEC _IsLockedAsyncProc = spIsLocked(_EmailListID,
+                                        _LockTypeAsyncProcess);
+
+  IF _IsLockedAsyncProc = 0
+  BEGIN
+    -- ASYNC PROCESS LOCK
+    EXEC _LockID = spLock(_EmailListID,
+                          _LockTypeListBuild,
+                          N'spAsyncListBuild',
+                          _ExternalUniqueID);
+  END;
+
+  COMMIT;
+
+  RETURN _LockID;
+
+END;
