6bc5ba60a999a655d58956b0112ee04e78012a98
pappala_naidu <papppala_naidu@persistent.com>
2024-01-24 12:28:03 +0000

    new commit error files

---
+++ pgsql_error/source/functions/fnLogToGraphite.sql
@@ -0,0 +1,8 @@
+ALTER FUNCTION public.fnlogtographite(
+    _data VARCHAR(4000),
+    _host VARCHAR(4000) = 'statsd.csnzoo.com',
+    _port INTEGER = 8125
+) RETURNS BIT
+LANGUAGE C
+AS '$libdir/graphite/graphite.so',
+    'LogToGraphite';
