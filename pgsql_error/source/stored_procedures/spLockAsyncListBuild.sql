c80312d6c4a19a7022d1643d218be0157e32aa4f
pappala_naidu <papppala_naidu@persistent.com>
2024-01-24 11:59:50 +0000

    new commit error files

---
+++ pgsql_error/source/stored_procedures/spLockAsyncListBuild.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE PROCEDURE public.splockasynclistbuild(IN _emaillistid integer, IN _procedurename VARCHAR(256), IN _externaluniqueid uuid, OUT _lockid integer)
+                     LANGUAGE plpgsql
+                    AS $procedure$
+                       DECLARE _locktypelistid INT = 1;
+                       DECLARE _locktypeasyncprocess INT = 2;
+                       DECLARE _islockedasyncproc BOOLEAN = true;
+                    BEGIN
+                       BEGIN TRANSACTION;
+                       SELECT spislocked(_emaillistid, _locktypeasyncprocess) INTO _islockedasyncproc;
+                       IF _islockedasyncproc = false THEN
+                          SELECT splock(_emaillistid, _locktypelistid, 'spasynclistbuild', _externaluniqueid) INTO _lockid;
+                       END IF;
+                       COMMIT;
+                       RETURN _lockid;
+                    END
+                    $procedure$
+                    ;
